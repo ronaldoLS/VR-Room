a38f9b306fee97969809b245dc51c0336770b869
Ronaldo Santos <65672971+ronaldoLS@users.noreply.github.com>
2026-07-29 20:02:36 +0100

    feat: Add contextual tooltips
    Updated Ronaldo Room.unity with many new instruction UI GameObjects (Hat, table, drawers, backgrounds) and wired ShowMessageFromList.NextMessage / SetActive persistent callbacks on various select/enter/exit events. Adjusted numerous prefab transforms and interactable/rigidbody settings (movement type, interpolation, collision detection) and other scene tweaks. Modified Assets/Scripts/Candle.cs to add a FlameEvent (UnityEvent<MonoBehaviour>), an OnFlameLit event and an isLit guard; the event is invoked when the candle detects a lit flame in OnTriggerStay. Also tweaked an OnVelocity threshold.

--- Assets/Scripts/Candle.cs
+++ Assets/Scripts/Candle.cs
@@ -1,42 +1,40 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Candle : MonoBehaviour
 {
     [SerializeField] private GameObject flame;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Serializable] public class FlameEvent : UnityEvent<MonoBehaviour> { }
+
+    public FlameEvent OnFlameLit = new FlameEvent();
+    bool isLit = false;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Flame"))
-        {
-            if(other.GetComponent<ParticleSystem>().isPlaying)            
-                flame.GetComponent<ParticleSystem>().Play();
-            
-        }
-    }
+
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Flame"))
         {
-            if(other.GetComponent<ParticleSystem>().isPlaying)
+            if (other.GetComponent<ParticleSystem>().isPlaying)
             {
                 flame.GetComponent<ParticleSystem>().Play();
-                Debug.Log("Candle is lit");
 
-            }         
+                if (!isLit)
+                    OnFlameLit.Invoke(this);
 
-            
-
+                isLit = true;
+            }
         }
     }
-
 }
