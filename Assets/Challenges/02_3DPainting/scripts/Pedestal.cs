b627df1c201a740a53fdfed8c31216e94269620c
Ronaldo Santos <65672971+ronaldoLS@users.noreply.github.com>
2026-08-06 17:27:51 +0100

    feat: 9. Add rotation slider for pedestal and update scene
    Expose a UI Slider on Pedestal (serialized field) and wire rotation to it. Update() now applies slider.value to the GameObject Y-rotation; added rotate() (uses slider value with clamp), testSlider() (debug log), and kept rotate(int). Scene file updated to add Slider UI elements (Rotation Slider, Handle, Fill, Background, Fill Area), hook the Slider's OnValueChanged to Pedestal.rotate, and flip attachToSpawnPoint. These changes enable runtime rotation control from the new UI slider.

--- Assets/Challenges/02_3DPainting/scripts/Pedestal.cs
+++ Assets/Challenges/02_3DPainting/scripts/Pedestal.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Pedestal : MonoBehaviour
 {
+    [SerializeField] private Slider slider;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,9 +15,18 @@ public class Pedestal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.rotation = Quaternion.Euler(0, slider.value, 0);
+    }
+    public void rotate()
+    {
+        transform.Rotate(Vector3.up, Mathf.Clamp(slider.value, 0, 360));
 
     }
-
+    public void testSlider()
+    {
+       Debug.Log("Rotating by angle: " + slider.value);
+        
+    }
     public void rotate(int angle)
     {
         transform.Rotate(Vector3.up, Mathf.Clamp(angle, -360, 360));
